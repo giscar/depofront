60fca15146e34e4bff559ef5beeeca06b0e65c8a
= <careli_2710@hotmail.com>
2024-05-09 07:46:33 -0500

    avace

--- src/componentes/servicio/ServicioNuevoComponent.jsx
+++ src/componentes/servicio/ServicioNuevoComponent.jsx
@@ -1,25 +1,42 @@
 import React, {useEffect, useState} from 'react'
 import BusquedaClienteComponent from '../cliente/BusquedaClienteComponent'
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { toast } from 'react-toastify';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { servicioSave } from '../../service/FacturaService';
 
 const ServicioNuevoComponent = () => {
 
-    const [codServicio, setCodServicio] = useState('')
-    const [ruc, setRuc] = useState('')
-    const [razonSocial, setRazonSocial] = useState('')
-    const [direccion, setDireccion] = useState('')
-    const [horaSalidaLocal, setHoraSalidaLocal] = useState('')
-    const [horaInicioServicio, setHoraInicioServicio] = useState('')
-    const [horaFinServicio, setHoraFinServicio] = useState('')
-    const [horaRetornoLocal, setHoraRetornoLocal] = useState('')
     const [cliente, setCliente] = useState([])
 
-    useEffect(() => {
-        console.log(cliente);
-        setRuc(cliente.ruc)
-        setDireccion(cliente.direccion)
-        setRazonSocial(cliente.razonSocial)
+    const notify = () => toast.info('Se han registrado los cambios correctamente', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+        });
 
-    }, [cliente])
+    const saveServicio= (data) => {
+        debugger;
+        data.codServicio = data.codServicio.toUpperCase();
+        data.ruc = data.ruc.toUpperCase();
+        data.razonSocial = data.razonSocial.toUpperCase();
+        data.direccion = data.direccion.toUpperCase();
+        data.horaSalidaLocal = data.horaSalidaLocal.toUpperCase();
+        data.horaInicioServicio = data.horaInicioServicio.toUpperCase();
+        data.horaFinServicio = data.horaFinServicio.toUpperCase();
+        data.horaRetornoLocal = data.horaRetornoLocal.toUpperCase();
+        data.estado = "1";
+        servicioSave(data).catch(error => {
+          console.error(error)
+        })
+        handleReset()
+        notify()
+      }
 
     //modal
     const [show, setShow] = useState(false);
@@ -27,136 +44,164 @@ const ServicioNuevoComponent = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const [errors, setErrors] = useState({
-        codServicio : '',
-        ruc : '',
-        horaSalidaLocal : '',
-        horaInicioServicio : '',
-        horaFinServicio : '',
-        horaRetornoLocal : ''
-    })
-
-    function saveOrUpdateServicio(){
-        validateForm()
-        
-    }
-
-    function validateForm(){
-        let valid = true;
-        const errorCopy = {... errors}
-
-        if (codServicio.trim()){
-            errorCopy.codServicio = '';
-        }else{
-            errorCopy.codServicio = 'Tiene que ingresar codigo de servicio';
-            valid = false;
-        }
-
-        if (ruc.trim()){
-            errorCopy.ruc = '';
-        }else{
-            errorCopy.ruc = 'Tiene que ingresar el ruc';
-            valid = false;
-        }
-
-        if (horaSalidaLocal.trim()){
-            errorCopy.horaSalidaLocal = '';
-        }else{
-            errorCopy.horaSalidaLocal = 'Tiene que ingresar la horaSalidaLocal';
-            valid = false;
-        }
-
-        if (horaInicioServicio.trim()){
-            errorCopy.horaInicioServicio = '';
-        }else{
-            errorCopy.horaInicioServicio = 'Tiene que ingresar la horaInicioServicio';
-            valid = false;
-        }
-
-        if (horaFinServicio.trim()){
-            errorCopy.horaFinServicio = '';
-        }else{
-            errorCopy.horaFinServicio = 'Tiene que ingresar la horaFinServicio';
-            valid = false;
-        }
-
-        if (horaRetornoLocal.trim()){
-            errorCopy.horaRetornoLocal = '';
-        }else{
-            errorCopy.horaRetornoLocal = 'Tiene que ingresar la horaRetornoLocal';
-            valid = false;
-        }
-
-        setErrors(errorCopy);
-
-        return valid;
-    }
-
+    const { handleSubmit, handleChange, handleReset, values, errors } = useFormik({
+        validationSchema: yup.object({
+            codServicio: yup.string().required(),
+            ruc: yup.string().required(),
+            razonSocial: yup.string().required(),
+            direccion: yup.string().required(),
+            horaSalidaLocal: yup.string().required(),
+            horaInicioServicio: yup.string().required(),
+            horaFinServicio: yup.string().required(),
+            horaRetornoLocal: yup.string().required()
+        }),
+        initialValues: {
+            codServicio: '',
+            ruc: '',
+            razonSocial: '',
+            direccion: '',
+            horaSalidaLocal: '',
+            horaInicioServicio: '',
+            horaFinServicio: '',
+            horaRetornoLocal: ''
+        },
+        onSubmit: saveServicio,
+      })
 
   return (
     <>
-    <div className='container'>
-        <div className='row'>
-            <div className='card'>
-                <h2>Nuevo Servicio</h2>
-                <div className='card-body'>
-                    <form>
-                        <div className='form-group md-2'>
-                            <label className='form-label'>codServicio:</label>
-                            <input type='text' placeholder='Ingrese el codServicio' name='txtCodServicio' value={codServicio} 
-                                className={`form-control ${errors.codServicio? 'is-invalid' : ''}`} onChange={(e) =>{setCodServicio(e.target.value)}}></input>
-                            {errors.codServicio && <div className='invalid-feedback'>{errors.codServicio}</div>}
-                        </div>
-                        <div className='form-group md-2'>
-                            <label className='form-label'>ruc:</label>
-                            <input type='text' placeholder='Ingrese el ruc' name='txtRuc' value={ruc} onClick={handleShow} readOnly
-                                className={`form-control ${errors.ruc? 'is-invalid' : ''}`} onChange={(e) =>{setRuc(e.target.value)}}></input>
-                            {errors.ruc && <div className='invalid-feedback'>{errors.ruc}</div>}
-                        </div>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>Razon Social:</label>
-                            <input type='text' placeholder='Ingrese el ruc' name='txRazonSocial' value={razonSocial} onClick={handleShow} disabled
-                                className={`form-control ${errors.ruc? 'is-invalid' : ''}`} onChange={(e) =>{setRazonSocial(e.target.value)}}></input>
-                            {errors.ruc && <div className='invalid-feedback'>{errors.RazonSocial}</div>}
-                        </div>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>Direccion:</label>
-                            <input type='text' placeholder='Ingrese el ruc' name='txtDireccion' value={direccion} onClick={handleShow} disabled
-                                className={`form-control ${errors.ruc? 'is-invalid' : ''}`} onChange={(e) =>{setDireccion(e.target.value)}}></input>
-                            {errors.ruc && <div className='invalid-feedback'>{errors.ruc}</div>}
-                        </div>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>horaSalidaLocal:</label>
-                            <input type='datetime-local' placeholder='Ingrese el horaSalidaLocal' name='txtHoraSalidaLocal' value={horaSalidaLocal} 
-                                className={`form-control ${errors.horaSalidaLocal? 'is-invalid' : ''}`} onChange={(e) => {setHoraSalidaLocal(e.target.value)}}></input>
-                            {errors.horaSalidaLocal && <div className='invalid-feedback'>{errors.horaSalidaLocal}</div>}
-                        </div>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>horaInicioServicio:</label>
-                            <input type='datetime-local' placeholder='Ingrese el horaInicioServicio' name='txtHoraInicioServicio' value={horaInicioServicio} 
-                                className={`form-control ${errors.horaInicioServicio? 'is-invalid' : ''}`} onChange={(e) => {setHoraInicioServicio(e.target.value)}}></input>
-                            {errors.horaInicioServicio && <div className='invalid-feedback'>{errors.horaInicioServicio}</div>}
-                        </div>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>horaFinServicio:</label>
-                            <input type='datetime-local' placeholder='Ingrese el horaFinServicio' name='txtHoraFinServicio' value={horaFinServicio} 
-                                className={`form-control ${errors.horaFinServicio? 'is-invalid' : ''}`} onChange={(e) => {setHoraFinServicio(e.target.value)}}></input>
-                            {errors.horaFinServicio && <div className='invalid-feedback'>{errors.horaFinServicio}</div>}
-                        </div>
-                        <div className='form-group mb-2'>
-                            <label className='form-label'>horaRetornoLocal:</label>
-                            <input type='datetime-local' placeholder='Ingrese el horaRetornoLocal' name='txtHoraRetornoLocal' value={horaRetornoLocal} 
-                                className={`form-control ${errors.horaRetornoLocal? 'is-invalid' : ''}`} onChange={(e) => {setHoraRetornoLocal(e.target.value)}}></input>
-                            {errors.horaRetornoLocal && <div className='invalid-feedback'>{errors.horaRetornoLocal}</div>}
-                        </div>
-                        
-                        <button className='btn btn-success' onClick={saveOrUpdateServicio}>Enviar</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <BusquedaClienteComponent show={show} handleClose={handleClose} setCliente={setCliente}/>
+      <Form noValidate onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationFormik01">
+            <Form.Label>Codigo del Servicicio</Form.Label>
+            <Form.Control
+              type="text"
+              name="codServicio"
+              value={values.codServicio}
+              onChange={handleChange}
+              isInvalid={!!errors.codServicio}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.codServicio}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik02">
+            <Form.Label>RUC</Form.Label>
+            <Form.Control
+              type="text"
+              name="ruc"
+              value={values.ruc}
+              onChange={handleChange}
+              isInvalid={!!errors.ruc}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.ruc}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik03">
+            <Form.Label>Razon Social</Form.Label>
+            <Form.Control
+              type="text"
+              name="razonSocial"
+              value={values.razonSocialrazonSocial}
+              onChange={handleChange}
+              isInvalid={!!errors.razonSocial}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.razonSocial}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik04">
+            <Form.Label>Direccion</Form.Label>
+            <Form.Control
+              type="text"
+              name="direccion"
+              value={values.direccion}
+              onChange={handleChange}
+              isInvalid={!!errors.direccion}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.direccion}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik05">
+            <Form.Label>Hora de salida de la Empresa</Form.Label>
+            <Form.Control
+              type="text"
+              name="horaSalidaLocal"
+              value={values.horaSalidaLocal}
+              onChange={handleChange}
+              isInvalid={!!errors.horaSalidaLocal}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.horaSalidaLocal}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik06">
+            <Form.Label>Hora de inicio del Servicio</Form.Label>
+            <Form.Control
+              type="text"
+              name="horaInicioServicio"
+              value={values.horaInicioServicio}
+              onChange={handleChange}
+              isInvalid={!!errors.horaInicioServicio}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.horaInicioServicio}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik07">
+            <Form.Label>Hora de fin del Servicio</Form.Label>
+            <Form.Control
+              type="text"
+              name="horaFinServicio"
+              value={values.horaFinServicio}
+              onChange={handleChange}
+              isInvalid={!!errors.horaFinServicio}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.horaFinServicio}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationFormik08">
+            <Form.Label>Hora de retorno a la empresa</Form.Label>
+            <Form.Control
+              type="text"
+              name="horaRetornoLocal"
+              value={values.horaRetornoLocal}
+              onChange={handleChange}
+              isInvalid={!!errors.horaRetornoLocal}
+              style={{ textTransform: 'uppercase' }}
+              autoComplete='off'
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.horaRetornoLocal}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4">
+            <Button type="submit" variant="info">Guardar</Button>
+            <Button type="reset" className='ms-2' onClick={() => handleReset()}
+          variant="warning">Limpiar
+        </Button>
+          </Form.Group>
+        </Row>
+        <br/>
+      </Form>
+      <BusquedaClienteComponent show={show} handleClose={handleClose} setCliente={setCliente}/>
     </>
   )
 }
